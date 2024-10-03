4868ae4e1a7883fbb8edb8f904a5673e4f4ffd91
fiesematente <mosesfinimento@googlemail.com>
2024-10-03 12:24:23 +0200

    Initial Commit

---
+++ DelegatePrinzip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegatePrinzip
+//
+//  Created by Mike Finimento on 03.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
